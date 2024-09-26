79acc8f05e857e7038418ec0acca12f3936a257a
josimarrenepont <jnrenepont@hotmail.com>
2024-09-25 22:17:05 -0300

    Updated Project

---
+++ data_analysis_project/scripts/main.py
@@ -0,0 +1,6 @@
+from scripts.tratamento_dados import tratar_dados
+from scripts.visualizacao_dados import gerar_visualizacoes
+
+if __name__ == "__main__":
+    tratar_dados()
+    gerar_visualizacoes()
